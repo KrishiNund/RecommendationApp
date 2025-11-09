4aec0edc66594d4a7f42c694eb24a74344f235e5
K_Nund <k.nund18@gmail.com>
2025-11-09 18:54:51 +0400

    -removed sitemap configs

--- rec-app/next.config.ts
+++ rec-app/next.config.ts
@@ -1,17 +1,6 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
-  async redirects() {
-    return [
-      {
-        source: '/:path*',
-        has: [{ type: 'host', value: 'recoards.com' }],
-        destination: 'https://www.recoards.com/:path*',
-        permanent: true,
-      },
-    ];
-  },
   // allow images from supabase domain
   images: {
     domains: ['hopysfaxmcfxmdpclhwq.supabase.co'],
